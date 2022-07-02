773e7aa2202de8e8e808a41eff8c8414348dc858
yakup salman <88798250+yakuphansalman@users.noreply.github.com>
2022-07-03 00:04:26 +0300

    21

--- Assets/Scripts/AI/AI_Sensor.cs
+++ Assets/Scripts/AI/AI_Sensor.cs
@@ -15,36 +15,71 @@ namespace AI
 
         #endregion
 
-        private Vector3[] _posDeltas;
+        [SerializeField] private Vector3[] _posDeltas;
+        [SerializeField] private float[] _posDeltaMagnitudes;
+
+        private int _activeScenario;
 
         private void Start()
         {
             _rays = new Ray[_rayCount];
             _hits = new RaycastHit[_rayCount];
             _posDeltas = new Vector3[_rayCount];
+            _posDeltaMagnitudes = new float[_rayCount];
         }
 
         private void FixedUpdate()
         {
             CastRays();
+            CheckObstacles();
         }
 
         private void CastRays()
         {
+            //0R,1F,2L,3FR,4O,5FL
+
+            float pi = 180 * Mathf.Deg2Rad;
             for (int i = 0; i < _rayCount; i++)
             {
-                for (int j = -1; j < 2; j++)//"j" represents left , origin or right
+                _rays[i] = new Ray(transform.position, 200 * (transform.right * Mathf.Round(Mathf.Cos((pi / 2) * (i % 3))) + transform.forward * Mathf.Round(Mathf.Sin(90 * (i % 2)))));
+                Physics.Raycast(_rays[i], out _hits[i], 200);
+
+                if (_activeScenario == i)
                 {
-                    for (int k = 0; k < 2; k++)//"k" represents origin or forward
-                    {
-                        if (k != 0 && j != 0)
-                        {
-                            _rays[j*k + j + k ] = new Ray(transform.position, transform.right * j + transform.forward * k);
-                        }
-                    }
-                    
+                    Debug.DrawRay(_rays[i].origin, _rays[i].direction * 200, new Color(0, 255, 0));
                 }
-                Debug.DrawRay(_rays[i].origin, _rays[i].direction * 30, new Color(0, 255, 0));
+                else
+                {
+                    Debug.DrawRay(_rays[i].origin, _rays[i].direction * 200, new Color(255, 0, 0));
+                }
+
+            }
+        }
+
+        private void CheckObstacles()
+        {
+            for (int i = 0; i < _rayCount; i++)
+            {
+                
+                if (_hits[i].collider != null)
+                {
+                    if (_hits[i].collider.tag == "Obstacle" || _hits[i].collider.tag == "Boundary")
+                    {
+                        _posDeltas[i] = _hits[i].collider.transform.localPosition - transform.position;
+                        _posDeltaMagnitudes[i] = _posDeltas[i].magnitude;
+                    }
+                }
+                if (i != 4)//4th member of rays is origin which has not direction
+                {
+                    if (Mathf.Min(_posDeltaMagnitudes) == 0)
+                    {
+
+                    }
+                    if (Mathf.Min(_posDeltaMagnitudes) == _posDeltaMagnitudes[i])
+                    {
+                        _activeScenario = i;
+                    }
+                }
             }
         }
     }
